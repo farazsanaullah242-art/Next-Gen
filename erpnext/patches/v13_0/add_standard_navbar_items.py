2fd19182a10e8ed0d485e2613c53e6dd4fc8f2a7
Faraz Sanaullah <farazsanaullah242@gmail.com>
2026-08-05 01:25:53 +0500

    feat: Next GEN - full rebranded ERPNext source

---
+++ erpnext/patches/v13_0/add_standard_navbar_items.py
@@ -0,0 +1,7 @@
+# import frappe
+from erpnext.setup.install import add_standard_navbar_items
+
+
+def execute():
+	# Add standard navbar items for Next GEN in Navbar Settings
+	add_standard_navbar_items()
